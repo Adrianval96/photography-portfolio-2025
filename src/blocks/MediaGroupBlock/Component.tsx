f942a55b7c2185b0ba1a7883e5ed48f8f19e818c
Adrian V <adrianval96@gmail.com>
2025-05-27 23:49:38 +1000

    conditionally render media in group block component

--- src/blocks/MediaGroupBlock/Component.tsx
+++ src/blocks/MediaGroupBlock/Component.tsx
@@ -26,16 +26,19 @@ export const MediaGroupBlock: React.FC<Props> = ({
 
   return (
     <div className={cn('media-group-container', className)}>
-      {mediaItems.map((item: MediaGroupItem, i: number) => (
-        <div key={i} className="media-group-item">
-          <Media imgClassName={cn('media-image', imgClassName)} resource={item.media} />
-          {item.caption && (
-            <div className={cn('media-caption', captionClassName)}>
-              <RichText data={item.caption} enableGutter={false} />
+      {mediaItems.map(
+        (item: MediaGroupItem, i: number) =>
+          item.media && (
+            <div key={i} className="media-group-item">
+              <Media imgClassName={cn('media-image', imgClassName)} resource={item.media} />
+              {item.caption && (
+                <div className={cn('media-caption', captionClassName)}>
+                  <RichText data={item.caption} enableGutter={false} />
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      ))}
+          ),
+      )}
     </div>
   )
 }
