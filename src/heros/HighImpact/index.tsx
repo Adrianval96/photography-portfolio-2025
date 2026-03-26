2902b9d782a26a5b716dd0bbcb3bfae86380a5e5
Adrian V <adrianval96@gmail.com>
2026-03-26 14:44:57 +1100

    Fix visual issues with landing page

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -17,7 +17,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
 
   return (
     <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+      className="relative -mt-[10.4rem] min-h-[calc(100vh+10.4rem)] flex items-center justify-center text-white"
       data-theme="dark"
     >
       <div className="container pt-24 mb-8 z-10 relative flex items-center justify-center">
@@ -36,9 +36,9 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
           )}
         </div>
       </div>
-      <div className="min-h-[80vh] select-none">
+      <div className="absolute inset-0 select-none">
         {media && typeof media === 'object' && (
-          <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+          <Media fill imgClassName="object-cover" pictureClassName="absolute inset-0" priority resource={media} />
         )}
       </div>
     </div>
