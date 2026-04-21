d73b5d9d7e95dca25d3db8b70c0a2d2f5517cdfc
Adri <adrianval96@gmail.com>
2026-04-21 14:02:49 +1000

    fix: force dynamic rendering on portfolio and homepage to guarantee fresh data

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
 import { FEATURED_WORK_LIMIT } from '@/constants'
+import { unstable_noStore } from 'next/cache'
 import { HeroSection } from './HeroSection'
 import { PositioningStrip } from './PositioningStrip'
 import { FeaturedWork } from './FeaturedWork'
@@ -8,6 +9,7 @@ import { CTASection } from './CTASection'
 import { About } from '@/components/About'
 
 export async function HomePage() {
+  unstable_noStore()
   const payload = await getPayload({ config: configPromise })
   const [
     { heroImage, heroHeadline, heroSubline, cta, positioningStatement, aboutSection, ctaSection },
