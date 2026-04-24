5ff20516171e9c8533061a919a3375c3e2c74df1
Adrian V <adrianval96@gmail.com>
2026-04-24 20:39:58 +1000

    [Refactor] Remove HOME_PAGE_SLUG — homepage fully driven by global

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,6 @@ export const DEFAULT_SERVER_URL = 'http://localhost:3000'
 
 export const REVALIDATE_SECONDS = 3600
 
-export const HOME_PAGE_SLUG = 'home'
-
 export const FEATURED_WORK_LIMIT = 6
 
 export const CACHE_TAG_PAGES_SITEMAP = 'pages-sitemap'
