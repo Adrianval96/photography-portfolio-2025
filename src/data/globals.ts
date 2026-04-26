8e64b1df2bbd346ac484d17dc948696a89155c79
Adrian V <adrianval96@gmail.com>
2026-04-26 18:32:41 +1000

    [Fix] WIP: save session changes

--- src/data/globals.ts
+++ src/data/globals.ts
@@ -26,7 +26,7 @@ const CACHE_KEYS = {
   socialLinks: 'global-social-links',
 } as const
 
-function makeGlobalFetch<S extends GlobalSlug>(slug: S, cacheKey: string, depth = 1) {
+function fetchFromCMS<S extends GlobalSlug>(slug: S, cacheKey: string, depth = 1) {
   return unstable_cache(
     async () => {
       const payload = await getPayload({ config: configPromise })
@@ -37,10 +37,13 @@ function makeGlobalFetch<S extends GlobalSlug>(slug: S, cacheKey: string, depth 
   )
 }
 
-export const fetchHomepageGlobal = makeGlobalFetch(GLOBAL_SLUGS.homepage, CACHE_KEYS.homepage)
-export const fetchContactGlobal = makeGlobalFetch(GLOBAL_SLUGS.contact, CACHE_KEYS.contact)
-export const fetchPortfolioPageGlobal = makeGlobalFetch(GLOBAL_SLUGS.portfolioPage, CACHE_KEYS.portfolioPage)
-export const fetchHeader = makeGlobalFetch(GLOBAL_SLUGS.header, CACHE_KEYS.header)
-export const fetchFooter = makeGlobalFetch(GLOBAL_SLUGS.footer, CACHE_KEYS.footer)
-export const fetchSiteIdentity = makeGlobalFetch(GLOBAL_SLUGS.siteIdentity, CACHE_KEYS.siteIdentity, 0)
-export const fetchSocialLinks = makeGlobalFetch(GLOBAL_SLUGS.socialLinks, CACHE_KEYS.socialLinks, 0)
+export const fetchHomepageGlobal = fetchFromCMS(GLOBAL_SLUGS.homepage, CACHE_KEYS.homepage)
+export const fetchContactGlobal = fetchFromCMS(GLOBAL_SLUGS.contact, CACHE_KEYS.contact)
+export const fetchPortfolioPageGlobal = fetchFromCMS(
+  GLOBAL_SLUGS.portfolioPage,
+  CACHE_KEYS.portfolioPage,
+)
+export const fetchHeader = fetchFromCMS(GLOBAL_SLUGS.header, CACHE_KEYS.header)
+export const fetchFooter = fetchFromCMS(GLOBAL_SLUGS.footer, CACHE_KEYS.footer)
+export const fetchSiteIdentity = fetchFromCMS(GLOBAL_SLUGS.siteIdentity, CACHE_KEYS.siteIdentity, 0)
+export const fetchSocialLinks = fetchFromCMS(GLOBAL_SLUGS.socialLinks, CACHE_KEYS.socialLinks, 0)
