bc3cb3b9672d616c100b82f93363fb850b321519
Adrian V <adrianval96@gmail.com>
2026-05-04 20:22:50 +1000

    [Fix] WIP: save session changes

--- src/utilities/__tests__/orientation.test.ts
+++ src/utilities/__tests__/orientation.test.ts
@@ -14,15 +14,5 @@ describe('getOrientation', () => {
     expect(getOrientation({ width: 800, height: 800 })).toBe('square')
   })
 
-  it('handles 1×1 as square', () => {
-    expect(getOrientation({ width: 1, height: 1 })).toBe('square')
-  })
 
-  it('handles very wide landscape ratios', () => {
-    expect(getOrientation({ width: 3000, height: 200 })).toBe('landscape')
-  })
-
-  it('handles very tall portrait ratios', () => {
-    expect(getOrientation({ width: 200, height: 3000 })).toBe('portrait')
-  })
 })
